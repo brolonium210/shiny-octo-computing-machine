901f8db6ca6b3703a5dbf3f2b34e2510a06cea8b
brolonium210 <parrwgrah@myvuw.ac.nz>
2024-02-26 19:09:19 +1300

    this is going ok need practice on vectors and stuff but it has been a good start

--- proj_01_main.cpp
+++ proj_01_main.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include "wav.hpp"
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main() {
+    int sample_rate = 44100;
+    double duration = 2.0;
+    int n_samples = sample_rate * duration;
+    std::vector<int> waveform;
+
+    int frequency0 = 340;
+    int frequency1 = 440;
+    int frequency2 = 540;    
+    int frequency3 = 640;
+    int frequency4 = 740;
+    int frequency5 = 840;
+    int frequency6 = 940;
+
+    int volume = 6000;
+//---------------------------------------------------------------------------------
+    for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency0 * t));
+    }
+
+    MakeWavFromVector("tone1.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency1 * t));
+    }
+
+    MakeWavFromVector("tone2.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency2 * t));
+    }
+
+    MakeWavFromVector("tone3.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency3 * t));
+    }
+
+    MakeWavFromVector("tone4.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency4 * t));
+    }
+
+    MakeWavFromVector("tone5.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+for (int i_sample = 0; i_sample < n_samples; i_sample++) {
+        double t = i_sample * 1.0 / sample_rate;
+        waveform.push_back(volume * sin(2 * M_PI * frequency5 * t));
+    }
+
+    MakeWavFromVector("tone6.wav", sample_rate, waveform);
+//---------------------------------------------------------------------------------
+
+
+
+
+
+
+
+    return 0;
 }
